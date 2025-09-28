bf77d6b361363638213fd631ff4694b611982d41
Miad008 <10694@holbertonstudents.com>
2025-09-28 17:30:34 +0000

    sort numbers in ascending order

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
+# Sort numbers and strings correctly
 
+puts ARGV.sort_by { |arg| Integer(arg) rescue arg }
 
-nums = ARGV.map(&:to_i)
-sorted = nums.sort
-sorted.each { |n| puts n }
